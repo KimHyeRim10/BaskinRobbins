3b6d9d953d13cb204307c6d5f0ea75b678943157
hong <test@test.com>
2024-05-24 16:23:33 +0900

    commit

---
+++ front/src/pages/Root.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer.jsx";
+import Header from "../components/Header.jsx";
+import Bebetter from "./Bebetter.jsx";
+import History from "./History.jsx";
+
+export default function Root() {
+  return (
+    <div>
+      <Header />
+      <Bebetter />
+      <Footer />
+    </div>
+  );
+}
