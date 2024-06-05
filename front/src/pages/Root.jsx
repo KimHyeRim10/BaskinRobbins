212e1a67d13ec58893bf8097e19411d0660c4699
hong <test@test.com>
2024-06-05 12:15:08 +0900

    commit

--- front/src/pages/Root.jsx
+++ front/src/pages/Root.jsx
@@ -1,10 +1,21 @@
 import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 export default function Root() {
+  function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+    return null;
+  }
+
   return (
     <div>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
