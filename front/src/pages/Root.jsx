99229f7c81f9201bd9d471a4481876cf36725575
hong <test@test.com>
2024-06-12 15:10:27 +0900

    commit

--- front/src/pages/Root.jsx
+++ front/src/pages/Root.jsx
@@ -2,6 +2,9 @@ import Footer from "../components/Footer.jsx";
 import Header from "../components/Header.jsx";
 import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons/faArrowUp";
+import "../css/pages.css";
 
 export default function Root({ cartCount }) {
   function ScrollToTop() {
@@ -18,6 +21,15 @@ export default function Root({ cartCount }) {
       <ScrollToTop />
       <Header cartCount={cartCount} />
       <Outlet />
+      <a href="#">
+        <FontAwesomeIcon
+          className="arrowup"
+          icon={faArrowUp}
+          bounce
+          size="lg"
+          style={{ color: "#f986bd" }}
+        />
+      </a>
       <Footer />
     </div>
   );
